11faa22b0647124ac592e244f684b167bd89794e
ishikaalunawat <63252702+ishikaalunawat@users.noreply.github.com>
2023-10-07 19:20:55 +0000

    Deploying to gh-pages from @ ishikaalunawat/ishikaalunawat.github.io@5a27185981900d6ec8c2b95df66bfb904b15adb9 🚀

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded",function(){document.getElementById("light-toggle").addEventListener("click",function(){toggleTheme(localStorage.getItem("theme"))})});
-=======
-document.addEventListener('DOMContentLoaded', function() {
-    const mode_toggle = document.getElementById("light-toggle");
-
-    mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
-    });
-});
-
->>>>>>> 454cef0 (Initial commit)
